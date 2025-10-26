5d39787ef091f605cfc6cc5d7ba1d316cc883ed4
Илья Горбачев <ilya-gorbachev01@yandex.ru>
2025-10-26 21:30:09 +0400

    refactoring

--- opinions_app.py
+++ opinions_app.py
@@ -1,6 +1,8 @@
+import csv
 from datetime import datetime
 from random import randrange
 
+import click
 from flask import Flask, abort, flash, redirect, render_template, url_for
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -15,6 +17,20 @@ app.config['SECRET_KEY'] = 'MY SECRET KEY'
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
+
+
+@app.cli.command('load_opinions')
+def load_opinions_command():
+    """Функция загрузки мнений в базу данных."""
+    with open('opinions.csv', encoding='utf-8') as f:
+        reader = csv.DictReader(f)
+        counter = 0
+        for row in reader:
+            opinion = Opinion(**row)
+            db.session.add(opinion)
+            db.session.commit()
+            counter += 1
+    click.echo(f'Загружено мнений: {counter}')
 
 
 class Opinion(db.Model):
